9d62b2fd9ae976d4378bb4aed4c2d3aff02fb6eb
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-06 16:40:48 -0500

    number check

--- 1.pl
+++ 1.pl
@@ -1,4 +1,9 @@
 sum_up_numbers_simple([], 0).
 sum_up_numbers_simple([Item1|Rest], Sum) :-
+    number(Item1),
     sum_up_numbers_simple(Rest, Rest_sum),
     Sum is Item1 + Rest_sum.
+sum_up_numbers_simple([Item1|Rest], Sum) :-
+    \+ number(Item1),
+    sum_up_numbers_simple(Rest, Rest_sum),
+    Sum is Rest_sum.
