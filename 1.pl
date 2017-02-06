dda12135251885f482faf3be479111c3d14f692b
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-06 15:50:44 -0500

    changed base case for null

--- 1.pl
+++ 1.pl
@@ -1,3 +1,3 @@
-sum-up-numbers-simple(L, [item]) :- length(L,1).
+sum-up-numbers-simple([], 0).
 sum-up-numbers-simple([item1,item2 | rest], sum) :-
-    sum-up-numbers-simple(item1+item2|rest).
+    sum-up-numbers-simple(item1+item2|rest, sum).
