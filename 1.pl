03a6e123125f8ed10f54c5a9a1cd6d14915278be
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-06 16:10:08 -0500

    variable naming rule Uppercase usage

--- 1.pl
+++ 1.pl
@@ -1,4 +1,4 @@
 sum-up-numbers-simple([], 0).
-sum-up-numbers-simple([item | rest], sum) :-
-    sum-up-numbers-simple(rest, rest_sum),
-    sum is item + rest_sum.
+sum-up-numbers-simple([Item1|Rest], Sum) :-
+    sum-up-numbers-simple(Rest, Rest_sum),
+    Sum is Item1 + Rest_sum.
