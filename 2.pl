4d9d5ffcf833de091b5b1b69b525813fb1626828
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-07 18:46:39 -0500

    minor update problems

--- 2.pl
+++ 2.pl
@@ -9,9 +9,10 @@ sum_up_numbers_general([Item1|Rest], Sum) :-
     sum_up_numbers_general(Rest, Rest_sum),
     Sum is Item1 + Rest_sum.
 
-% if first index is a number, and sum of rest of list matches rest_sum, sum is first item + rest_sum
+% if first index is a list, sum of rest of list matches rest_sum, sum of
+% first index list matches list_sum, sum is list_sum + rest_sum
 sum_up_numbers_general([Item1|Rest], Sum) :-
-    % number check
+    % list check
     is_list(Item1),
     %this recursive call gets iterated until list is null and uses the logic if null, sum = 0 from above
     sum_up_numbers_general(Item1, List_sum),
@@ -20,9 +21,10 @@ sum_up_numbers_general([Item1|Rest], Sum) :-
 
 
 
-% if first index is not a number, and sum of rest of list matches rest_sum, sum is first rest_sum
+% if first index is not a number or a list, and sum of rest of list
+% matches rest_sum, sum is first rest_sum
 sum_up_numbers_general([Item1|Rest], Sum) :-
-    % number check
+    % number and list check
     \+ number(Item1),
     \+ is_list(Item1),
     sum_up_numbers_general(Rest, Rest_sum),
