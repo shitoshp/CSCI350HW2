927d0e58811d2390c2cfe410ff1c44e14eba71a3
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-08 13:40:33 -0500

    check if L2 member is in L1

--- 4.pl
+++ 4.pl
@@ -6,3 +6,8 @@ common_unique_elements([Head|Tail], L2, [Head|Tail1]) :-
     member(Head, L2),
     \+ member(Head, Tail1),
     common_unique_elements(Tail, L2, Tail1).
+
+common_unique_elements(L1, [Head|Tail], [Head|Tail1]) :-
+    member(Head, L1),
+    \+ member(Head, Tail1),
+    common_unique_elements(L1, Tail, Tail1).
--- 4.pl~
+++ 4.pl~
@@ -1,3 +1,8 @@
 common_unique_elements([], _, []).
 
 common_unique_elements(_, [], []).
+
+common_unique_elements([Head|Tail], L2, [Head|Tail1]) :-
+    member(Head, L2),
+    \+ member(Head, Tail1),
+    common_unique_elements(Tail, L2, Tail1).
