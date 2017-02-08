19dece9b3f7b67aec8c7075825a1a03d395b721a
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-08 14:28:35 -0500

    documentation

--- 4.pl
+++ 4.pl
@@ -3,11 +3,13 @@ common_unique_elements([], _, []).
 
 common_unique_elements(_, [], []).
 
+%if new element of L1 is in L2, then it is in output list
 common_unique_elements([Head|Tail], L2, [Head|Tail1]) :-
     member(Head, L2),
-
     common_unique_elements(Tail, L2, Tail1).
 
+% if first element if L1 is not in L2, then the output list remains as
+% it is
 common_unique_elements([Head|Tail], L2, Tail1) :-
     \+ member(Head, L2),
     common_unique_elements(Tail, L2, Tail1).
