604bed3f084cd4d6eb47625c2e70611201dc0303
Shitosh Parajuli <shitosh@yahoo.com>
2017-02-08 14:22:56 -0500

    removed nests checking since code not working

--- 4.pl
+++ 4.pl
@@ -5,14 +5,11 @@ common_unique_elements(_, [], []).
 
 common_unique_elements([Head|Tail], L2, [Head|Tail1]) :-
     member(Head, L2),
+
     common_unique_elements(Tail, L2, Tail1).
 
 common_unique_elements([Head|Tail], L2, Tail1) :-
     \+ member(Head, L2),
     common_unique_elements(Tail, L2, Tail1).
-
-remove_nest([], []).
-
-
 
 
